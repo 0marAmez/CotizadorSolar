e4d4ee24a365affa6abcf61cc1a258a8d152b191
0marAmez <omar11.amezquita@gmail.com>
2023-10-18 21:25:05 -0500

    pop window

--- widget.py
+++ widget.py
@@ -41,22 +41,47 @@ def create_label_and_entry(root, label_text, entry_bg, entry_fg, entry_width, la
 def open_promedio_bimestral_window():
     # Create a new window
     promedio_window = tk.Toplevel(root)
+    # Set the width and height as needed
+    promedio_window.geometry("600x700")
+    promedio_window.configure(bg="#FCD12A")  # Set your desired background color  
 
-    # Create labels and entries in the new window
-    create_label_and_entry(promedio_window, "Label 1", "white", "black", 30, 15, 15, 15, 50)
-    create_label_and_entry(promedio_window, "Label 2", "white", "black", 30, 15, 60, 15, 95)
-    create_label_and_entry(promedio_window, "Label 3", "white", "black", 30, 15, 105, 15, 140)
+    # Bimestre I
+    create_label_and_entry(promedio_window, "Bimestre I", "white", "black", 30, 65, 15, 15, 50)
+    create_label_and_entry(promedio_window, "Periodo del Bimestre I", "white", "black", 30, 305, 15, 305, 50)
+    # Bimestre II
+    create_label_and_entry(promedio_window, "Bimestre II", "white", "black", 30, 65, 115, 15, 150)
+    create_label_and_entry(promedio_window, "Periodo del Bimestre II", "white", "black", 30, 305, 115, 305, 150)
+    # Bimestre III
+    create_label_and_entry(promedio_window, "Bimestre III", "white", "black", 30, 65, 215, 15, 250)
+    create_label_and_entry(promedio_window, "Periodo del Bimestre III", "white", "black", 30, 305, 215, 305, 250)
+    # Bimestre IV
+    create_label_and_entry(promedio_window, "Bimestre IV", "white", "black", 30, 65, 315, 15, 350)
+    create_label_and_entry(promedio_window, "Periodo del Bimestre IV", "white", "black", 30, 305, 315, 305, 350)
+    # # Bimestre V
+    create_label_and_entry(promedio_window, "Bimestre V", "white", "black", 30, 65, 415, 15, 450)
+    create_label_and_entry(promedio_window, "Periodo del Bimestre V", "white", "black", 30, 305, 415, 305, 450)
+    # # Bimestre VI
+    create_label_and_entry(promedio_window, "Bimestre VI", "white", "black", 30, 65, 515, 15, 550)
+    create_label_and_entry(promedio_window, "Periodo del Bimestre VI", "white", "black", 30, 305, 515, 305, 550)
 
     # Create a button to close the new window
-    close_button = tk.Button(promedio_window, text="Close", command=promedio_window.destroy)
-    close_button.place(x=15, y=175)
+    close_button = tk.Button(promedio_window, text="Cerrar", command=promedio_window.destroy)
+    close_button.place(x=450, y=600)
 
 def on_button_click():
     # Saca los valores del widget
     cliente = entry_widgets["Nombre del recibo"].get()
+    direccion = entry_widgets["Direccion"].get()
+    no_cotizacion = entry_widgets["No. de Cotizacion"].get()
+    no_servicio = entry_widgets["Numero de Servicio"].get()
+    vendedor = entry_widgets["Vendedor"].get()
     print(cliente)
+    print(direccion)
+    print(no_cotizacion)
+    print(no_servicio)
+    print(vendedor)
     # direccion = entry3.get()
-    # numero_de_servicio = entry4.get()
+    # numero_de_servicio = entry4.get()s
     # vendedor = entry5.get()
     # promedios_kw = entry6.get()
 
@@ -146,29 +171,28 @@ if __name__ == "__main__":
     # Numero de Servicio
     create_label_and_entry(root, "Numero de Servicio", "white", "black", 35, 330, 230, 300, 260)
 
-    # Tarifa
-    # text_label7 = tk.Label(root, text="Tarifa", bg="#FCD12A", font=("Arial", 12, "bold"), fg="black")
-    # text_label7.place(x=45, y=330)  # Set the position of the label
-    # options = ["01", "PBDT", "DAC", "GDMTO"]
-    # selected_option = tk.StringVar(root)
-    # selected_option.set(options[0])  # Set the default selected option
-    # option_menu = tk.OptionMenu(root, selected_option, *options, command=on_option_selected)
-    # option_menu.config(width=15, anchor="center", bg="white", fg="black")  # Set background and text color of the menu
-    # option_menu["menu"].config(bg="white", fg="black")  # Set background and text color of the dropdown menu
-    # option_menu.place(x=45, y=370, width=200)
+    # Tarifa (Opciones)
+    text_label_tarifa = tk.Label(root, text="Tarifa", bg="#FCD12A", font=("Arial", 12, "bold"), fg="black")
+    text_label_tarifa.place(x=85, y=330)  # Set the position of the label
+    options = ["01", "PBDT", "DAC", "GDMTO"]
+    selected_option = tk.StringVar(root)
+    selected_option.set(options[0])  # Set the default selected option
+    option_menu = tk.OptionMenu(root, selected_option, *options, command=on_option_selected)
+    option_menu.config(width=15, anchor="center", bg="white", fg="black")  # Set background and text color of the menu
+    option_menu["menu"].config(bg="white", fg="black")  # Set background and text color of the dropdown menu
+    option_menu.place(x=15, y=360, width=200)
 
-    # Create the input box (Numero De Servicio)
-    # text_label5 = tk.Label(root, text="No. de Servicio", bg="#FCD12A", font=("Arial", 12, "bold"), fg="black")
-    # text_label5.place(x=335, y=200)  # Set the position of the label
-    # entry4 = tk.Entry(root,bg="white",highlightthickness=0, relief=tk.FLAT,fg="black",width=30)
-    # entry4.place(x=300, y=230)
+    # Create a button to open the pop-up window for the Promedio the kwh
+    text_label_Promedio = tk.Label(root, text="Promedio", bg="#FCD12A", font=("Arial", 12, "bold"), fg="black")
+    text_label_Promedio.place(x=370, y=330)  # Set the position of the label
+    promedio_button = tk.Button(root, text="Editar Promedio", command=open_promedio_bimestral_window)
+    promedio_button.place(x=305, y=360, width=210, height=30)
 
-    # Create a button to open the pop-up window
-    promedio_button = tk.Button(root, text="Edit Promedio", command=open_promedio_bimestral_window)
-    promedio_button.place(x=225, y=370)
+    # Vendedor
+    create_label_and_entry(root, "Vendedor", "white", "black", 35, 220, 430, 160, 460)
 
     # Create a button to read the input and print it
-    button = tk.Button(root, bg="#D3D3D3", fg="black", text="Submit", command=on_button_click, font=("Arial", 12, "bold"))
+    button = tk.Button(root, bg="#D3D3D3", fg="black", text="Cerrar", command=on_button_click, font=("Arial", 12, "bold"))
     button.place(x=550, y=500,width=100, height=40)
 
 
